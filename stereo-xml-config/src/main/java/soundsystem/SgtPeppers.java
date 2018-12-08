6ddff2b2e0697ab6cca913fa1192425c31136f54
guohailiang <592827060@qq.com>
2018-12-08 20:02:06 +0800

    add the xmlconfig way to achieve import Beans

---
+++ stereo-xml-config/src/main/java/soundsystem/SgtPeppers.java
@@ -0,0 +1,15 @@
+package soundsystem;
+
+public class SgtPeppers implements CompactDisc {
+
+  private String title = "Sgt. Pepper's Lonely Hearts Club Band";  
+  private String artist = "The Beatles";
+  
+//  public void play() {
+//    System.out.println("Playing " + title + " by " + artist);
+//  }
+    public void play() {
+    System.out.println("123");
+  }
+
+}
